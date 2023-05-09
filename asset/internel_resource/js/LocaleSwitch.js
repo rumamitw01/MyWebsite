16240d085b08379009e59e7e08272c970bf01162
琉見 <42371341+rumamitw01@users.noreply.github.com>
2023-05-09 15:35:31 +0800

    Update LocaleSwitch.js

--- asset/internel_resource/js/LocaleSwitch.js
+++ asset/internel_resource/js/LocaleSwitch.js
@@ -38,7 +38,7 @@ function Locale_Switch(locale){
 
 var locale=navigator.language;
 locale=locale.toLowerCase();
-var requestURL="./asset/internel_resource/locale/supported_locale.json";
+var requestURL="/asset/internel_resource/locale/supported_locale.json";
 var request=new XMLHttpRequest();
 request.open("get",requestURL);
 request.responseType = 'json';
